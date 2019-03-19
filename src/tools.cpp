c2afdb43311a1ef4c7461271b13a0b60e8e6a421
Pranay Kumar <Pranay.kumar@infosys.com>
2019-03-19 18:37:30 +0530

    Add files via upload

---
+++ src/tools.cpp
@@ -0,0 +1,107 @@
+#include "tools.h"
+#include <iostream>
+
+using Eigen::VectorXd;
+using Eigen::MatrixXd;
+using std::vector;
+using std::cout;
+using std::endl;
+
+Tools::Tools() {}
+
+Tools::~Tools() {}
+
+VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
+                              const vector<VectorXd> &ground_truth) {
+  /**
+   * TODO: Calculate the RMSE here.
+   */
+
+  //Intialize the rmse
+  VectorXd rmse(4);
+  rmse << 0,0,0,0;
+
+  float t = estimations.size(); // Current timestep index
+
+  //The estimation vector size should not be zero and the estimation vector size should equal ground truth vector size
+  if(estimations.size() != ground_truth.size() || estimations.size() == 0){
+      cout << "Invalid estimation or ground_truth data" << endl;
+      return rmse;
+  }
+
+  //Accumulate squared residuals
+  for(unsigned int i=0; i < estimations.size(); ++i){
+      VectorXd residual = estimations[i] - ground_truth[i];
+      
+      //Coefficient-wise multiplication
+      residual = residual.array()*residual.array();
+      rmse += residual;
+  }
+
+
+  //Calculate the mean
+  rmse = rmse / estimations.size();
+
+  //Calculate the squared root
+  rmse = rmse.array().sqrt();
+
+  if (rmse(0) > .11 || rmse(1) > .11 || rmse(2) > .52 || rmse(3) > .52){
+      cout << "Root Mean Square Value crossing the benchmark threshold value at timestep " << t << " rmse: " << rmse << endl;
+  }
+		
+  if (rmse(0) > .11){
+       cout << "rmse(0) exceeds tolerance of .11" << endl;
+  }
+		
+  if (rmse(1) > .11){
+        cout << "rmse(1) exceeds tolerance of .11" << endl;
+  }
+		
+  if (rmse(2) > .52){
+        cout << "rmse(2) exceeds tolerance of .52" << endl;
+  }
+		
+  if (rmse(3) > .52){
+        cout << "rmse(3) exceeds tolerance of .52" << endl;
+  }
+		
+
+  return rmse;
+
+}
+
+MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
+  /**
+   * TODO:
+   * Calculate a Jacobian here.
+   */
+   
+   MatrixXd Hj(3,4);
+
+
+   //Recover state parameters
+  float px = x_state(0);
+  float py = x_state(1);
+  float vx = x_state(2);
+  float vy = x_state(3);
+
+
+  //Pre-compute a set of terms to avoid repeated calculation
+  float c1 = px*px+py*py;
+  float c2 = sqrt(c1);
+  float c3 = (c1*c2);
+
+
+  //Check division by zero
+  if (fabs(c1) < 0.0001){
+      std::cout << "CalculateJacobian () - Error - Division by Zero" << std::endl;
+      return Hj;
+  }
+
+  //Compute the Jacobian matrix
+  Hj << (px / c2), (py / c2), 0, 0,
+      -(py / c1), (px / c1), 0, 0,
+      py*(vx*py - vy*px) / c3, px*(px*vy - py*vx) / c3, px / c2, py / c2;
+
+  return Hj;
+}
